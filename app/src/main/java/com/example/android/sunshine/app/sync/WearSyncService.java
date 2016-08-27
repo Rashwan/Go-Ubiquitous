248ac9b98b642602c1dac56c016e9fe75fd88299
Rashwan <ahmed.t.rashwan@gmail.com>
2016-08-27 21:42:10 +0200

    Implement weather updates from handheld to wear device

--- app/src/main/java/com/example/android/sunshine/app/sync/WearSyncService.java
+++ app/src/main/java/com/example/android/sunshine/app/sync/WearSyncService.java
@@ -2,15 +2,23 @@ package com.example.android.sunshine.app.sync;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Log;
 
+import com.example.android.sunshine.app.Utility;
+import com.example.android.sunshine.app.data.WeatherContract;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayOutputStream;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -22,7 +30,15 @@ public class WearSyncService extends IntentService{
     private static final int GOOGLE_API_CLIENT_TIMEOUT_S = 10;
     private static final String TAG = WearSyncService.class.getSimpleName();
     private static final String GOOGLE_API_CLIENT_ERROR_MSG = "Failed to connect to GoogleApiClient (error code = %d)";
-
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+    };
+    // these indices must match the projection
+    private static final int INDEX_WEATHER_ID = 0;
+    private static final int INDEX_MAX_TEMP = 1;
+    private static final int INDEX_MIN_TEMP = 2;
 
     public WearSyncService() {
         super(TAG);
@@ -44,8 +60,38 @@ public class WearSyncService extends IntentService{
         ConnectionResult connectionResult = googleApiClient.blockingConnect(
                 GOOGLE_API_CLIENT_TIMEOUT_S, TimeUnit.SECONDS);
         if (connectionResult.isSuccess() && googleApiClient.isConnected()) {
+
+            String location = Utility.getPreferredLocation(this);
+            Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                    location, System.currentTimeMillis());
+            Cursor data = getContentResolver().query(weatherForLocationUri, FORECAST_COLUMNS, null,
+                    null, WeatherContract.WeatherEntry.COLUMN_DATE + " ASC");
+            if (data == null) {
+                return;
+            }
+            if (!data.moveToFirst()) {
+                data.close();
+                return;
+            }
+            // Extract the weather data from the Cursor
+            int weatherId = data.getInt(INDEX_WEATHER_ID);
+            int weatherArtResourceId = Utility.getArtResourceForWeatherCondition(weatherId);
+            double maxTemp = data.getDouble(INDEX_MAX_TEMP);
+            double minTemp = data.getDouble(INDEX_MIN_TEMP);
+            String formattedMaxTemperature = Utility.formatTemperature(this, maxTemp);
+            String formattedMinTemperature = Utility.formatTemperature(this, minTemp);
+            data.close();
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),weatherArtResourceId);
+            Asset asset = createAssetFromBitmap(bitmap);
+
             PutDataMapRequest dataMap = PutDataMapRequest.create("/weather");
+
+            dataMap.getDataMap().putString("MIN_TEMP",formattedMinTemperature);
+            dataMap.getDataMap().putString("MAX_TEMP",formattedMaxTemperature);
+            dataMap.getDataMap().putAsset("IMAGE",asset);
             dataMap.getDataMap().putString("KEY", String.valueOf(Math.random()));
+
             PutDataRequest request = dataMap.setUrgent().asPutDataRequest();
             DataApi.DataItemResult result = Wearable.DataApi.putDataItem(googleApiClient, request)
                     .await();
@@ -60,5 +106,10 @@ public class WearSyncService extends IntentService{
             Log.e(TAG, String.format(GOOGLE_API_CLIENT_ERROR_MSG,
                     connectionResult.getErrorCode()));
         }
+    }
+    private Asset createAssetFromBitmap(Bitmap bitmap) {
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+        return Asset.createFromBytes(byteStream.toByteArray());
     }
 }
